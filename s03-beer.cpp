68593f1307be5f5368446fd640ed80aaeeb4d956
Student <s25663@pjwstk.edu.pl>
2021-10-22 10:57:08 +0200

    s03-piwsko

---
+++ s03-beer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+auto main() -> int{
+	
+	for(int i=99;i>0;i--){
+		std::cout << i << " bottles of beer on the wall, \n" << i << " bottles of beer. Take one down, pass it around, ";
+
+		}
+	
+	std::cout << "\nNo more bottles of beer on the wall, no more bottles of beer. \n";
+	std::cout << "Go to the store and buy some more, 99 bottles of beer on the wall... \n";
+	
+	
+	return 0;
+	}
