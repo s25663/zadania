890712b5c7abfd2d43c98bb2d1a38a728e782e77
Student <s25663@pjwstk.edu.pl>
2021-10-22 09:58:49 +0200

    powrot

---
+++ s03-guessing-game.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+
+auto main() -> int
+{
+	int rand();
+	
+	auto pifpaf = std::string{};
+	
+	srand(time(NULL));
+	auto losik = rand()% 100 +1;
+	
+	std::getline(std::cin; pifpaf);
+	auto a = std::stoi(pifpaf);	
+	
+	if(a>losik)
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
