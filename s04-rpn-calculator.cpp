cb09e87c02443f9a30eb6e87abf9b0a97ec4f5d1
filip2 <s25663@pjwstk.edu.pl>
2022-01-03 12:13:59 +0100

    kalkulator!

--- s04-rpn-calculator.cpp
+++ s04-rpn-calculator.cpp
@@ -1,42 +1,47 @@
 #include <iostream>
 #include <string>
 #include <math.h>
+#include <string.h>
 
-auto main(int argc, char * argv[]) -> int{
+auto main(int argc, char* argv[]) -> int{
 	
-	int a = stoi(argv[1]);
-	int b = stoi(argv[2]);
-	char znak = argv[3];
+	int a = std::stoi(argv[1]);
+	int b = std::stoi(argv[2]);
+	auto znak = argv[3];
+	double a1 = 0;
+	double b1 = 0;
 	
-	switch (znak){
-	
-	case '+':
+	if (strcmp( znak, "+") == 0){
 		std::cout << a << " + " << b << " = " << a+b;
-		break;
-	
-	case '-':
-		std::cout << a << " - " << b << " = " << a-b;
-		break;
-	case '*':
-		std::cout << a << " * " << b << " = " << a*b;
-		break;
-	case '/':
-		double a1 = a*1.0;
-		double b1 = b*1.0;
-		std::cout << a1 << " / " << b1 << " = " << a1/b1;
-		break;
-	case '//':
-		std::cout << a << " / " << b << " = " << a/b;
-		break;
-	case '**':
-		std::cout << a << " do potęgi " << b << " = " << pow(a, b);
-		break;
-	case 'sqrt':
-		std::cout << " pierwiastek kwadratowy z "<< a << "i " << b << " wynosi " << sqrt(a) << " " << sqrt(b);
-		break;
-	
-	
 	}
+	
+	else if (strcmp( znak, "-") == 0){
+		std::cout << a << " - " << b << " = " << a-b;
+	}
+	else if (strcmp( znak, "*") == 0){
+		std::cout << a << " * " << b << " = " << a*b;
+	}
+	else if (strcmp( znak, "/") == 0){
+		a1 = a*1.0;
+		b1 = b*1.0;
+		std::cout << a1 << " / " << b1 << " = " << a1/b1;
+	}
+		
+	else if (strcmp( znak, "//") == 0){
+		std::cout << a << " / " << b << " = " << a/b;
+	}
+	else if (strcmp( znak, "**") == 0){
+		std::cout << a << " do potęgi " << b << " = " << pow(a, b);
+	}
+	else if (strcmp( znak, "sqrt") == 0){
+		std::cout << " pierwiastek kwadratowy z "<< a << " i " << b << " wynosi " << sqrt(a) << " " << sqrt(b);
+	}
+	else{
+		std::cout << "Coś namieszałeś/namieszałaś, spróbuj ponownie i wpisz poprawnie - cyfrę cyfrę działanie";
+		}
+	
+		return 0;
+	
 	
 }
 
